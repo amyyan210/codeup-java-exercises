9186500d9253ac0d1b71e45615354ed53545dfe1
amyyan210 <amyyan210@gmail.com>
2017-09-26 15:04:58 -0500

    Start on arrays exercise

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,19 @@
+public class ServerNameGenerator {
+    String[] adjectives = {"pretty", "ugly", "nice", "mean", "deserving", "hopeful", "messy", "clean", "useful", "scary"};
+    String[] nouns = {"table", "chair", "laptop", "apartment", "cat", "friend", "city", "teacher", "leg", "face"};
+
+
+    public String getString() {
+//        for (String word: partOfSpeech) {
+//            return "Your " + partOfSpeech + " is " + word + ".";
+//        }
+
+        return adjectives.toString();
+    }
+
+//    public static void main(String[] args) {
+//        System.out.println(getString(adjectives));
+//        System.out.println(getString(nouns));
+
+//    }
+}
