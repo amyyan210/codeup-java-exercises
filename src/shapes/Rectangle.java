aac88d004d213de5e670950a9d8d8451db81b723
amyyan210 <amyyan210@gmail.com>
2017-09-28 11:40:25 -0500

    Started rectangle/square exercise

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,19 @@
+package shapes;
+
+public class Rectangle {
+    private float length;
+    private float width;
+
+    public Rectangle (float length, float width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public float getArea () {
+        return length * width;
+    }
+
+    public float getPerimeter () {
+        return (2 * length) + (2 * width);
+    }
+}
---
+++ src/shapes/Square.java
@@ -0,0 +1,18 @@
+package shapes;
+
+public class Square extends Rectangle {
+    private float side;
+
+    public Square (float side) {
+        super (side, side);
+    }
+
+    public Square getArea {
+
+    }
+
+    public Square getPerimeter {
+
+    }
+
+}
