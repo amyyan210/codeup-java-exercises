9a9e413845a366189d6ee5835353103ca17ae523
amyyan210 <amyyan210@gmail.com>
2017-09-29 12:12:02 -0500

    Finish exercise

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,27 +1,50 @@
 package shapes;
 
-public class Rectangle {
-    private float length;
-    private float width;
+public class Rectangle extends Quadrilateral implements Measurable {
 
-    public Rectangle (float length, float width) {
+    public Rectangle (double length, double width) {
+        super (length, width);
+    }
+
+    @Override
+    public void setLength(double length) {
         this.length = length;
-        this.width = width;
     }
 
-    public float getArea () {
-        return length * width;
+    @Override
+    public void setWidth(double width) { this.width = width; }
+
+    @Override
+    public double getPerimeter() {
+        return (2 * this.length) + (2 * this.width);
     }
 
-    public float getPerimeter () {
-        return (2 * length) + (2 * width);
+    @Override
+    public double getArea() {
+        return this.length * this.width;
     }
+//    private float length;
+//    private float width;
+//
+//    public Rectangle (float length, float width) {
+//        this.length = length;
+//        this.width = width;
+//    }
+//
+//    public float getArea () {
+//        return length * width;
+//    }
+//
+//    public float getPerimeter () {
+//        return (2 * length) + (2 * width);
+//    }
+//
+//    public float getLength() {
+//        return length;
+//    }
+//
+//    public float getWidth() {
+//        return width;
+//    }
 
-    public float getLength() {
-        return length;
-    }
-
-    public float getWidth() {
-        return width;
-    }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,19 +1,49 @@
 package shapes;
 
-public class Square extends Rectangle {
+public class Square extends Quadrilateral {
 
-    public Square (float side) {
+    public Square (double side) {
         super (side, side);
     }
 
-    public float getArea () {
-        //makes reference to square class, use super.getLength() etc. if you want that method
-        return this.getLength() * this.getWidth();
+    @Override
+    public void setLength(double side) {
+        this.length = side;
+        this.width = side;
     }
 
-    public float getPerimeter () {
-        //makes reference to square class, use super.getLength() etc. if you want that method
-        return 4 * this.getWidth();
+    @Override
+    public void setWidth(double side) {
+        this.length = side;
+        this.width = side;
     }
+
+    @Override
+    public double getPerimeter() {
+        return this.length * 4;
+    }
+
+    @Override
+    public double getArea() {
+        return length * length;
+    }
+
+//    public Square (float side) {
+//        super (side, side);
+//    }
+//
+//    public float getArea () {
+//        //makes reference to rectangle class, use super.getLength() etc. if you want that method
+//        return this.getLength() * this.getWidth();
+//    }
+//
+//    public float getPerimeter () {
+//        //makes reference to rectangle class, use super.getLength() etc. if you want that method
+//        return 4 * this.getWidth();
+//    }
+
+
+
+
 
 }
